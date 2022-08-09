011cad90306fd6647eb5dbaf83c0b030de27637f
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-09 18:33:17 -0300

    feat: preparing script to run migrations and seeds

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const error = (err, _req, res, _next) => {
   console.log(err);
   return res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
