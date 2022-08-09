8f1b7ca3fa98529d9b3cea576bcceac0b554d2bd
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-09 12:58:59 -0300

    feat: installing helmet

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const error = require('./middlewares/error');
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 
 app.use(error);
