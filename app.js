a519384da196da97fff16ec6402e95034d34f0d3
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-09 13:05:26 -0300

    feat: install: rate-limit, rescue, express, mysql2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+// const rescue = require('express-rescue');
+const limiter = require('./middlewares/limiter');
+require('dotenv').config();
 const error = require('./middlewares/error');
 
 const app = express();
 app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 
 app.use(error);
