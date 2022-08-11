285e70b433b87bf0eaa2bc7f5985ad5b097ebe47
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-11 14:07:57 -0300

    feat: installing morgan to log application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const rescue = require('express-rescue');
 const swaggerUi = require('swagger-ui-express');
+const morgan = require('morgan');
 const limiter = require('./middlewares/limiter');
 require('dotenv').config();
 const error = require('./middlewares/error');
@@ -13,6 +14,7 @@ const app = express();
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
+app.use(morgan('common'));
 
 app.use('/sales', rescue(salesRouter));
 app.use('/products', rescue(productsRouter));
